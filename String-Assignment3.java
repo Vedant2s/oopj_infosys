2f73d94dc8f9280d566115d06c27c33cab0c797b
Vedant2s <98942578+Vedant2s@users.noreply.github.com>
2023-03-21 19:26:28 +0530

    Update String-Assignment3.java

--- String-Assignment3.java
+++ String-Assignment3.java
@@ -4,14 +4,25 @@ class Tester {
 	    String b="",c="";
 	    int a=0;
 	    for(int i=0;i<str.length();i++){
-	        if(str.charAt(i)==' '||i==str.length()-1){
+	        if(str.charAt(i)==' '){
 	            for(int c1=i-1;c1>=a;c1--){
 	                if(c1==i-1)
 	                b=String.valueOf(str.charAt(c1));
 	                else
 	                b=b+str.charAt(c1);
-	                if(c1==a)
+	                if(c1==a&&c1!=str.length()-1)
 	                b=b+str.charAt(i);
+	            }
+	            c=c.concat(b);
+	            a=i+1;
+	        }
+	        if(i==str.length()-1){
+	            for(int c1=i;c1>=a;c1--){
+	                if(c1==i)
+	                b=String.valueOf(str.charAt(c1));
+	                else
+	                b=b+str.charAt(c1);
+	                
 	            }
 	            c=c.concat(b);
 	            a=i+1;
